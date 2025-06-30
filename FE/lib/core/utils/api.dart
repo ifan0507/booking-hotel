88e862239c107bf2a2dac53c33ce7e679dc335fa
BimaWijaya69 <catfly765@gmail.com>
2025-06-30 16:29:46 +0700

    push tablist boo

--- FE/lib/core/utils/api.dart
+++ FE/lib/core/utils/api.dart
@@ -2,7 +2,7 @@ import 'package:get_storage/get_storage.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
 
 class Api {
-  String baseUrl = "http://192.168.201.26:8080";
+  String baseUrl = "http://192.168.87.244:8080";
 
   Map<String, String> getToken() {
     final box = GetStorage();
